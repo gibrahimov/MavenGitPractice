656de8c19319faab6b5ea20f70e318934ea5d69f
madiyard <icloudka@gmail.com>
2022-08-17 09:38:32 -0700

    Changes from Madiyar

--- src/main/java/com/cydeo/Test1.java
+++ src/main/java/com/cydeo/Test1.java
@@ -3,5 +3,11 @@ package com.cydeo;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello G1");
+
+        System.out.println("New line from Madiyar");
+
+
     }
+
+
 }
